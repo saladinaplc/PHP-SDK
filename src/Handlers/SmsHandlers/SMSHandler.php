d394b0a33d61115f58342ef896b06a2af93916cd
MakamuEvans <makamuevans@gmail.com>
2020-06-12 12:13:02 +0300

    Invalid Sender Corrected

--- src/Handlers/SmsHandlers/SMSHandler.php
+++ src/Handlers/SmsHandlers/SMSHandler.php
@@ -18,7 +18,7 @@ trait SMSHandler
         $uri = "$this->version/send-sms";
         try {
             $message = [$sms];
-            return (new SMSHandlerImpl($this->token, $uri, ...$message))->process();
+            return (new SMSHandlerImpl($this->token, $uri, $message))->process();
         } catch (\Exception $e) {
             return $e->getMessage();
         }
