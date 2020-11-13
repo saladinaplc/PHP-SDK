61ad9c75b29de452702861eb06afffee73abf4f8
MakamuEvans <makamuevans@gmail.com>
2020-11-13 16:53:45 +0300

    url update

--- src/Handlers/BongaHandler.php
+++ src/Handlers/BongaHandler.php
@@ -12,7 +12,7 @@ use GuzzleHttp\RequestOptions;
 
 abstract class BongaHandler
 {
-    const BASE_URL = "https://bulk.bongatech.co.ke/api/";
+    const BASE_URL = "http://bulk.bongatech.com/api/";
     //const BASE_URL = "http://127.0.0.1:8000/api/";
 
     const METHOD_POST = "sendPostRequest";
